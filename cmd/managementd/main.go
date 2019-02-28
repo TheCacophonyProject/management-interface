0f9d763c56cb7d689db6caa4f5309a82c2a222bc
GP <g.ferraro22@gmail.com>
2019-02-28 14:37:50 +1300

    merged with upstream

--- cmd/managementd/main.go
+++ cmd/managementd/main.go
@@ -59,15 +59,9 @@ func main() {
 
 	// UI handlers.
 	router.HandleFunc("/", managementinterface.IndexHandler).Methods("GET")
-<<<<<<< HEAD
-	router.HandleFunc("/network-interfaces", managementinterface.NetworkInterfacesHandler).Methods("GET")
-	router.HandleFunc("/wifi-networks", managementinterface.WifiNetworkHandler).Methods("GET","POST")
-
+	router.HandleFunc("/wifi-networks", managementinterface.WifiNetworkHandler).Methods("GET", "POST")
 	router.HandleFunc("/wifi-networks/{id}", managementinterface.DeleteNetworkHandler).Methods("DELETE")
-
-=======
 	router.HandleFunc("/network", managementinterface.NetworkHandler).Methods("GET")
->>>>>>> upstream/master
 	router.HandleFunc("/interface-status/{name:[a-zA-Z0-9-* ]+}", managementinterface.CheckInterfaceHandler).Methods("GET")
 	router.HandleFunc("/online-state", managementinterface.ToggleOnlineState).Methods("POST")
 	router.HandleFunc("/speaker", managementinterface.SpeakerTestHandler).Methods("GET")
