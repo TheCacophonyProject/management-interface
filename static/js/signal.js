f883b8d88a8e1d753a0dbf5a022fd90942abe0e7
Zaza <gferraro>
2019-03-27 16:41:27 +1300

    added signal svg

--- static/js/signal.js
+++ static/js/signal.js
@@ -1,32 +1,68 @@
-window.onload = function() {
-  updateSignalLoop()
-};
+updateSignal();
 
-async function updateSignalLoop() {
-  const refreshTime =2 * 1000 
+const refreshTime =2 * 1000
+const clearSignalAttempts = 3 
+const maxStrength = 100;
+const minStrength = 0;
+const bars = 5;
+const signalRange = maxStrength - minStrength
+const signalPerBar = signalRange / parseFloat(bars);
 
+var signalFails = 0;
+
+async function updateSignal() {
   var xmlHttp = new XMLHttpRequest();
   xmlHttp.open("GET", "/api/signal-strength", true);
   xmlHttp.setRequestHeader("Authorization", "Basic "+btoa("admin:feathers"))
+  
   xmlHttp.onload = async function() {
+    var signalElement = document.getElementById("signal-strength");
     if (xmlHttp.status == 200) {
-      var signalElement = document.getElementById("signal-strength");
-      signalElement.innerText = xmlHttp.response;
-      await sleep(refreshTime);
+      handleSignalSuccess(xmlHttp.response);
     } else {
-      console.log("status:", xmlHttp.status);
-      console.log("response:", xmlHttp.response);
-      await sleep(refreshTime);
+      handleSignalFailure("status:" + xmlHttp.status + " response:" + xmlHttp.response);
     }
-    updateSignalLoop(); 
-  }
-  xmlHttp.onerror = function(err) {
-    console.log('error:', err);
+    reCheckSignalIn(refreshTime); 
   }
 
-  xmlHttp.send( null );
+  xmlHttp.onerror = function() {
+    handleFailure("error occured accesing " + "/api/signal-strength")
+    reCheckSignalIn(refreshTime); 
+  }
+
+  xmlHttp.send(null);
 }
 
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
+function handleSignalSuccess(signalVal){
+  signalFails = 0;
+  var signalElement = document.getElementById("signal-strength");
+  var strength =parseInt(signalVal);
+
+  signalElement.innerText = strength;
+  var signalBars = Math.ceil(strength / signalPerBar);
+  for(var i = 1; i <= bars; i++){
+    var bar = $(".signal-" + i);
+    if(i <= signalBars){
+      bar.addClass("signal")
+      bar.removeClass("no-signal")
+    }else{
+      bar.addClass("no-signal")
+      bar.removeClass("signal")
+    }
+  }
+}
+
+function handleSignalFailure(errorMessage){
+  if(signalFails == 0){
+    console.log(errorMessage);
+  }
+  signalFails++;
+  if(signalFails >= clearSignalAttempts){
+    var signalElement = document.getElementById("signal-strength");
+    signalElement.innerText = " - "
+  }
+}
+
+function reCheckSignalIn(ms) {
+  setTimeout(updateSignal, ms);
 }
