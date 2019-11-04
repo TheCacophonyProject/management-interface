5e39529d73c5c5ea0e881c84fe1348c06cb5c62d
cam <cameron.ryan.pears@gmail.com>
2019-11-04 15:11:05 +1300

    Update clock page

--- go.mod
+++ go.mod
@@ -5,9 +5,9 @@ go 1.12
 require (
 	github.com/TheCacophonyProject/go-api v0.0.0-20190923033957-174cea2ac81c
 	github.com/TheCacophonyProject/go-config v0.0.0-20191003001220-bff132a03491
+	github.com/TheCacophonyProject/rtc-utils v1.2.0
 	github.com/gobuffalo/packr v1.30.1
 	github.com/godbus/dbus v4.1.0+incompatible
 	github.com/gorilla/context v1.1.1 // indirect
 	github.com/gorilla/mux v1.6.2
-	gopkg.in/yaml.v2 v2.2.2
 )
---
+++ static/js/clock.js
@@ -0,0 +1,43 @@
+"use strict";
+window.onload = function() {
+  getState()
+}
+
+async function getState() {
+  try {
+    var response = await apiGetJSON('/api/clock');
+    console.log(response)
+    $("#rtc-date").html(response.RTCTime)
+    $("#system-date").html(response.SystemTime)
+    if (response.LowRTCBattery) {
+      $("#rtc-battery").html("Low/Empty. Replace soon.")
+    } else {
+      $("#rtc-battery").html("OK.")
+    }
+    if (response.RTCIntegrity) {
+      $("#rtc-integrity").html("True.")
+    } else {
+      $("#rtc-integrity").html("False. Don't trust time from RTC")
+    }
+    if (response.NTPSynced) {
+      $("#ntp-synced").html("True.")
+    } else {
+      $("#ntp-synced").html("False.")
+    }
+  } catch(e) {
+    console.log(e)
+  }
+}
+
+async function setTime() {
+  var now = new Date()
+  var data = {"date": now.toISOString()}
+  try {
+    await apiFormURLEncodedPost('/api/clock', data)
+    alert("udpated time")
+    getState()
+  } catch(e) {
+    console.log(e);
+    alert("failed to update time")
+  }
+}
