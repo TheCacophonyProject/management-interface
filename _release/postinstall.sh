b093820572a49e617a3097eb1ccd3d6733cd1eae
Cameron <cameron.ryan.pears@gmail.com>
2025-08-19 15:36:59 +1200

    Don't restart the service, it will be restarted by salt instead at end of update

--- _release/postinstall.sh
+++ _release/postinstall.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 systemctl daemon-reload
 systemctl enable managementd.service
-systemctl restart managementd.service
+
+# Using start instead of restart to avoid restarting the service if it's already running, causing API issues during a salt update.
+systemctl start managementd.service
